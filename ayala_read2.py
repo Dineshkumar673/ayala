bb8d1ab975840db178006ce4f9334c7eda00dc47
Dinesh Kumar T <76119910+Dineshkumar673@users.noreply.github.com>
2021-11-02 09:53:11 +0530

    Update ayala_read2.py

--- ayala_read2.py
+++ ayala_read2.py
@@ -49,7 +49,7 @@ for i in range (0,len(directory_contents)):
             input_frame = cv2.imread(path+'/'+directory_contents[i]+'/'+class_name[j]+'/'+img_name[k])
             
             #print(input_frame)
-            with mp_pose.Pose(static_image_mode=True,min_detection_confidence=0.37,model_complexity=1) as pose:
+            with mp_pose.Pose(static_image_mode=True,min_detection_confidence=0.37,model_complexity=0) as pose:
                 pose_result = pose.process(image=input_frame)
                 pose_landmarks = pose_result.pose_landmarks
 
